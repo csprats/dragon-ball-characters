562bcaadf11e689d1490c641b3d8c612e3522ab8
csprats <222044220+csprats@users.noreply.github.com>
2025-08-28 17:05:25 +0200

    Add b

--- src/Character.jsx
+++ src/Character.jsx
@@ -3,9 +3,9 @@ export const Character = ({name, ki, image, afilliation, race}) => {
         <div className="character">
             <img src={image} alt={name} className="image"/>
             <h2>{name}</h2>
-            <p>Ki: {ki}</p>
-            <p>Affiliation: {afilliation}</p>
-            <p>Race: {race}</p>
+            <p>Ki: <b>{ki}</b></p>
+            <p>Affiliation: <b>{afilliation}</b></p>
+            <p>Race: <b>{race}</b></p>
         </div>
     )
     //            <p>Afilliation: {afilliation}</p>
