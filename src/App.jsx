f7f2a837830522d148272329f40ba735862af3af
csprats <222044220+csprats@users.noreply.github.com>
2025-08-28 10:52:56 +0200

    Add more

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ export const App = () => {
   const [character, setCharacter] = useState([])
   const [character2, setCharacter2] = useState([])
   const [character3, setCharacter3] = useState([])
+  const [character4, setCharacter4] = useState([])
+  const [character5, setCharacter5] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,6 +25,16 @@ export const App = () => {
         const result3 = await response3.json()
         setCharacter3(result3.items) // <--- Aquí guardamos directamente el arreglo de personajes
         console.log(result3.items)
+
+        const response4 = await fetch('https://dragonball-api.com/api/characters?page=4&limit=10')
+        const result4 = await response4.json()
+        setCharacter4(result4.items) // <--- Aquí guardamos directamente el arreglo de personajes
+        console.log(result4.items)
+
+        const response5 = await fetch('https://dragonball-api.com/api/characters?page=5&limit=10')
+        const result5 = await response5.json()
+        setCharacter5(result5.items) // <--- Aquí guardamos directamente el arreglo de personajes
+        console.log(result5.items)
       } catch (error) {
         console.error("Error fetching data: ", error)
       }
@@ -50,6 +62,12 @@ export const App = () => {
       ))}
       {character3.map((chr3, i3) => (
           <Character key={i3} name={chr3.name} ki={chr3.ki} maxki={chr3.maxKi} image={chr3.image} afilliation={chr3.affiliation}/>
+      ))}
+      {character4.map((chr4, i4) => (
+          <Character key={i4} name={chr4.name} ki={chr4.ki} maxki={chr4.maxKi} image={chr4.image} afilliation={chr4.affiliation}/>
+      ))}
+      {character5.map((chr5, i5) => (
+          <Character key={i5} name={chr5.name} ki={chr5.ki} maxki={chr5.maxKi} image={chr5.image} afilliation={chr5.affiliation}/>
       ))}
     </>
   )
